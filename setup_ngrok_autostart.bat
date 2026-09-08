b53724411fe05fb66ea0c157e59965c7084ee0e9
Dicky Rachmat Iskandar <drachmat.iskandar@gmail.com>
2026-09-08 18:39:49 +0700

    fix: update ngrok autostart target port to 3000

--- setup_ngrok_autostart.bat
+++ setup_ngrok_autostart.bat
@@ -14,7 +14,7 @@ if %errorLevel% neq 0 (
 echo @echo off > C:\automova\start_ngrok.bat
 echo taskkill /f /im ngrok.exe >nul 2>&1
 echo timeout /t 2 /nobreak >nul 2>&1
-echo start "" "%%USERPROFILE%%\AppData\Local\Microsoft\WindowsApps\ngrok.exe" http 80 >> C:\automova\start_ngrok.bat
+echo start "" "%%USERPROFILE%%\AppData\Local\Microsoft\WindowsApps\ngrok.exe" http 3000 >> C:\automova\start_ngrok.bat
 
 :: Register the task in Windows Task Scheduler to run on boot
 schtasks /create /tn "StartNgrokWeb" /tr "C:\automova\start_ngrok.bat" /sc ONSTART /ru "adminpabrik" /rp "pabrik123" /rl HIGHEST /f
